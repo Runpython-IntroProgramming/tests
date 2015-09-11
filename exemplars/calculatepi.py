3c417935985cfeb18ce9d547364abd922191d59b
Eric Dennison <ericd@netdenizen.com>
2015-09-10 21:49:01 -0400

    Update calculatepi.py

--- exemplars/calculatepi.py
+++ exemplars/calculatepi.py
@@ -1,4 +1,4 @@
 n = int(input("I will estimate pi. How many terms should I use? "))
-sigfigs = int(input("How many sig figs should I use in the result? "))
+sigfigs = int(input("How many decimal places should I use in the result? "))
 pi = 4*sum([((-1)**k)/(2.0*k+1) for k in range(0,n)])
-print("The approximate value of pi is {0:.{precision}}".format(pi,precision=sigfigs))
+print("The approximate value of pi is {0:.{1}f}".format(pi,sigfigs))
