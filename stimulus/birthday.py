91c756c3724487898f7f85edf27e6a57600c9038
Eric Dennison <ericd@netdenizen.com>
2015-09-14 21:04:20 -0400

    Update birthday.py

--- stimulus/birthday.py
+++ stimulus/birthday.py
@@ -1,11 +1,22 @@
 # stimulus for birthday quiz
 import random
+from datetime import datetime
+from calendar import month_name
 
 trials = 10
 random.seed()
 halloweentrial = random.randint(0, 8)
 birthdaytrial = halloweentrial+1
-name = random.choice(['Tom', 'Sue', 'Rob', 'Rebecca', 'William', 'Emma'])month = random.choice(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
-year = random.randint(1970, 2014)
-date = random.randint(1,31)
-print("{}\n{}\n".format(name, age))
+for trial in range(trials):
+  name = random.choice(['Tom', 'Sue', 'Rob', 'Rebecca', 'William', 'Emma'])
+  year = random.randint(1970, 2014)
+  if trial == halloweentrial:
+    date = 31
+    month = 'October'
+  elif trial == birthdaytrial:
+    date = datetime.today().day
+    month = month_name[datetime.today().month]
+  else:
+    month = random.choice(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
+    date = random.randint(1,31)
+  print("{}\n{}\n{}\n{}\n".format(name, month, year, date))
