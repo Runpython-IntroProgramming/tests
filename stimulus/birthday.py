b6369a9d23b92b90b84b70094274705fc5d7b336
Eric Dennison <ericd@netdenizen.com>
2015-09-14 21:00:44 -0400

    Update birthday.py

--- stimulus/birthday.py
+++ stimulus/birthday.py
@@ -1,1 +1,11 @@
 # stimulus for birthday quiz
+import random
+
+trials = 10
+random.seed()
+halloweentrial = random.randint(0, 8)
+birthdaytrial = halloweentrial+1
+name = random.choice(['Tom', 'Sue', 'Rob', 'Rebecca', 'William', 'Emma'])month = random.choice(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
+year = random.randint(1970, 2014)
+date = random.randint(1,31)
+print("{}\n{}\n".format(name, age))
