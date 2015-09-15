0d106b4319f0381c9823494ab98ae93592d41d93
Eric Dennison <ericd@netdenizen.com>
2015-09-14 21:09:26 -0400

    Update birthday.py

--- stimulus/birthday.py
+++ stimulus/birthday.py
@@ -5,18 +5,16 @@ from calendar import month_name
 
 trials = 10
 random.seed()
-halloweentrial = random.randint(0, 8)
-birthdaytrial = halloweentrial+1
-for trial in range(trials):
-  name = random.choice(['Tom', 'Sue', 'Rob', 'Rebecca', 'William', 'Emma'])
-  year = random.randint(1970, 2014)
-  if trial == halloweentrial:
-    date = 31
-    month = 'October'
-  elif trial == birthdaytrial:
-    date = datetime.today().day
-    month = month_name[datetime.today().month]
-  else:
-    month = random.choice(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
-    date = random.randint(1,31)
-  print("{}\n{}\n{}\n{}\n".format(name, month, year, date))
+trialversion = random.randint(1,3) # pick one of three...
+name = random.choice(['Tom', 'Sue', 'Rob', 'Rebecca', 'William', 'Emma'])
+year = random.randint(1970, 2014)
+if trialversion == halloweentrial:
+  date = 31
+  month = 'October'
+elif halloweentrial == 2:
+  date = datetime.today().day
+  month = month_name[datetime.today().month]
+else:
+  month = random.choice(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
+  date = random.randint(1,31)
+print("{}\n{}\n{}\n{}\n".format(name, month, year, date))
