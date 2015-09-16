20f022f6343e7ec16b9bc5b9b444f3f12c1cf156
Eric Dennison <ericd@netdenizen.com>
2015-09-15 21:52:29 -0400

    Create fizzbuzz.py

---
+++ exemplars/fizzbuzz.py
@@ -0,0 +1,14 @@
+maxn = int(input("How many numbers shall we print? "))
+fizzn = int(input("For multiples of what number shall we print 'Fizz'? "))
+buzzn = int(input("For multiples of what number shall we print 'Buzz'? "))
+for n in range(1,maxn+1):
+    fizzmultiple = not n % fizzn # % calculates remainder of division - zero if n is multiple of fizzn
+    buzzmultiple = not n % buzzn # ditto..
+    if fizzmultiple and buzzmultiple:   # check for both, first
+        print("FizzBuzz")
+    elif fizzmultiple:  # then individuals
+        print("Fizz")
+    elif buzzmultiple:
+        print("Buzz")
+    else:
+        print(n)
