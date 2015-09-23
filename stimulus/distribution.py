99301a65e95dfcfc607d1eae1047266151f7a800
Eric Dennison <ericd@netdenizen.com>
2015-09-23 11:34:28 -0400

    Create distribution.py

---
+++ stimulus/distribution.py
@@ -0,0 +1,13 @@
+import random
+random.seed()
+samplestrings = ["How now? A rat? Dead, for a ducat, dead!",
+  "Done to death by slanderous tongue",
+  "Why then tonight let us assay our plot",
+  "Thou art a votary to fond desire",
+  "Some Cupid kills with arrows, some with traps",
+  "Be not afraid of greatness",
+  "Lord, what fools these mortals be",
+  "Our remedies oft in ourselves do lie",
+  "I go, and it is done; the bell invites me"]
+
+print(random.choice(samplestrings))
