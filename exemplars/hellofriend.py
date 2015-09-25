fa8a22fe0c68f965d8fffb74ef6c328093424c17
Eric Dennison <ericd@netdenizen.com>
2015-09-25 15:21:20 -0400

    Update hellofriend.py

--- exemplars/hellofriend.py
+++ exemplars/hellofriend.py
@@ -4,7 +4,6 @@ year = datetime.datetime.now().year
 
 name = input("Please tell me your name: ")
 age = input("Please tell me your age: ")
-# age = int(age)
-age = 34  # remove!
+age = int(age)
 pythonage = year - 1991
 print("Hello, {0}. Python is {1} years older than you are!".format(name, pythonage-age))
