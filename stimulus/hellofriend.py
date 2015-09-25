fb16dc194ec3aa8a7b78f48ddb72ef4a66215ab7
Eric Dennison <ericd@netdenizen.com>
2015-09-25 15:22:57 -0400

    Update hellofriend.py

--- stimulus/hellofriend.py
+++ stimulus/hellofriend.py
@@ -1,6 +1,6 @@
 import random
 
-rndom.seed()
+random.seed()
 name = random.choice(['Tom', 'Sue', 'Rob', 'Rebecca', 'William', 'Emma'])
 age = random.randint(10,18)
 print("{}\n{}\n".format(name, age))
