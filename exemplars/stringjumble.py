30af9b5a26b84d7ef9715f796fc14925d03e0f09
Eric Dennison <ericd@netdenizen.com>
2015-09-27 13:48:11 -0400

    Create stringjumble.py

---
+++ exemplars/stringjumble.py
@@ -0,0 +1,9 @@
+rawtext = input("Please enter a string of text (the bigger the better): ")
+allreversed = rawtext[::-1]
+allwords = rawtext.split()
+allwordsinreverse = allwords[::-1]
+allwordsreversed = [word[::-1] for word in allwords]
+print ('You entered "{0}". Now jumble it: '.format(rawtext))
+print(rawtext[::-1])
+print(' '.join(allwordsinreverse))
+print(' '.join(allwordsreversed))
